7c93a31a6460f5667e5a67c11100474f1360a355
alphanoobie <hussaingadi.hg@gmail.com>
2023-02-13 13:34:37 +0530

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Layout } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Layout>
+        <Header>
+
+        </Header>
+
+        <Content>
+
+        </Content>
+
+        <Footer>
+          
+        </Footer>
+      </Layout>
+    </>
   );
 }
 
