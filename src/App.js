700adbd7faaeadd4ea2885e268ded280e0b1ceca
alphanoobie <hussaingadi.hg@gmail.com>
2023-02-13 23:35:00 +0530

    created data context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import { Layout } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 import Banner from "./components/Banner";
 import DataLayout from "./components/DataLayout";
 import HeaderContent from "./components/HeaderContent";
 import SearchBar from "./components/SearchBar";
 
+export const CapsuleContext = createContext();
+
 function App() {
+  const [capsuleData, setCapsuleData] = useState([]);
+  const value = {capsuleData, setCapsuleData}
+
+  const getData = async () => {
+    const data = await axios.get("https://api.spacexdata.com/v3/capsules");
+    setCapsuleData(data.data);
+  };
+
+  useEffect(() => {
+    getData();
+
+  }, []);
+
   return (
-    <>
+    <CapsuleContext.Provider value={value}>
       <Layout>
         <Header>
           <HeaderContent />
@@ -21,7 +38,7 @@ function App() {
 
         <Footer></Footer>
       </Layout>
-    </>
+    </CapsuleContext.Provider>
   );
 }
 
--- src/components/DataLayout.js
+++ src/components/DataLayout.js
@@ -1,26 +1,18 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import DataCard from "./DataCard";
 import { useState } from "react";
 import { Spin } from "antd";
+import { CapsuleContext } from "../App";
 
 export default function DataLayout() {
-  const [capsuleData, setCapsuleData] = useState([]);
+  const {capsuleData, setCapsuleData} = useContext(CapsuleContext);
   const [loading, setLoading] = useState(true);
 
-  const getData = async () => {
-    const data = await axios.get("https://api.spacexdata.com/v3/capsules");
-    setCapsuleData(data.data);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
+  
   return (
     <div className="m-6 p-6 bg-white rounded">
-      {loading ? (
+      {!loading ? (
         <div className="flex justify-center">
           <Spin />
         </div>
